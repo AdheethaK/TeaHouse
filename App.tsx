5a480c138bf35e3e668a42b4439410d83833e241
ANK <kalupahanaadheetha5@gmail.com>
2024-07-18 08:32:31 +0530

    second commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View ,Button , Alert , Text, StyleSheet, Pressable } from 'react-native';
 
-export default function App() {
+export default function App(props) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>HI!</Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      <Pressable style={styles.button} onPress={() => Alert.alert('hi again')}>
+      <Text style={styles.text}>{'get started'}</Text>
+    </Pressable>
       <StatusBar style="auto" />
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFCEAD',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    position : 'absolute',
+    bottom : 10,
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    color : 'black',
+    elevation: 3,
+    backgroundColor: '#ADECFF',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
