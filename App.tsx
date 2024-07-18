cf97148e8cb6a8ca72d0e30bc443ea45e3f1cf71
ANK <kalupahanaadheetha5@gmail.com>
2024-07-18 12:15:20 +0530

    add logo to Welcome Screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import { Image } from 'expo-image';
 import { View ,Button , Alert , Text, StyleSheet, Pressable } from 'react-native';
+
+const blurhash =
+  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
 export default function App(props) {
   return (
     <View style={styles.container}>
-      <Text>HI!</Text>
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-      <Pressable style={styles.button} onPress={() => Alert.alert('hi again')}>
-      <Text style={styles.text}>{'get started'}</Text>
-    </Pressable>
-      <StatusBar style="auto" />
+
+      <Text style={styles.text}>COFFEE</Text>
+      <View style={styles.logoContainer}>
+        <Image 
+          style={styles.image} 
+          source={require('./assets/images/LOGO.png')} 
+          placeholder={{ blurhash }}
+          contentFit="cover"
+          transition={1000}
+        />
+        {/* <Text>HOUSE</Text> */}
+      </View>
+
     </View>
   );
   
@@ -21,7 +29,7 @@ export default function App(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFCEAD',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -37,11 +45,22 @@ const styles = StyleSheet.create({
     elevation: 3,
     backgroundColor: '#ADECFF',
   },
+  logoContainer: {
+    backgroundColor: 'white',
+    width : 280,
+    height : 280,
+  } ,
   text: {
-    fontSize: 16,
-    lineHeight: 21,
+    fontSize: 30,
+    lineHeight: 50,
     fontWeight: 'bold',
     letterSpacing: 0.25,
-    color: 'white',
+    color: '817B7E',
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height : 'auto' ,
+    backgroundColor: 'white',
   },
 });
